e6bfbafac0e87c4225f9bbe0d3fda506bc3e879b
dxzdxz77 <76502041+dxzdxz77@users.noreply.github.com>
2021-01-07 11:53:28 +0900

    0107

--- js/main.js
+++ js/main.js
@@ -14,5 +14,36 @@ $('#header_top .ui_btn').click(function(){
     }
 })//언어 선택 열고 닫기
 
+$('#key_popup .close').click(function(){
+    $('#key_popup').css({display:'none'});
+})//공지 팝업창 닫기
 
+/*$(document).ready(function() {
+var wd = $(window);
+$('#slide .paral').each(function(){
+  var bg = $(this);
+  wd.scroll(function(){
+    var yPos = -(wd.scrollTop() / 2); 
+      //나눠지는 숫자가 작을 수록 속도가 빠름
+    var coords = '50%' + yPos + 'px';
+    bg.css({backgroundPosition:coords});
+  });
+});//패럴*/
+
+$('.footer_ui_btn').click(function(event){
+  event.preventDefault();
+  $("html,body").stop().animate({scrollTop:0});
+
+  if ($(document).scrollTop() >= $('#sec1').offset().top) {
+    $('.footer_ui_btn').fadeIn(); }
+    else{  $('.footer_ui_btn').fadeOut();
+    }
+})
+
+$(window).scroll(function(){
+if ($(document).scrollTop() >= $('#sec1').offset().top) {
+  $('.footer_ui_btn').fadeIn(); }
+  else{ $('.footer_ui_btn').fadeOut();
+}
+})//맨 위로 스크롤링
 })//jp end
